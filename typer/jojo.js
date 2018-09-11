7aedb94a0889ba32c1fc0363652577bdc2c3f4d2
facedesk <dgold@sjnma.org>
2018-09-11 14:32:52 -0500

    Update jojo.js

--- typer/jojo.js
+++ typer/jojo.js
@@ -15,9 +15,14 @@ $( "body" ).keypress(function( event ) {
   }
   else if (event.which == 13){
   		$("#typed").text(typed);
-        var missed = levenshteinDistance(secret,typed);
+        var missed = levenshtein(secret,typed)
+        
+        secret.length - typed.length;
+        
         var hit = secret.length - missed;
         var percent = hit / secret.length;
+
+    
         $("#missed").text("missed="+missed);
         $("#hit").text("hit="+hit);
         $("#percent").text("percent="+percent);
@@ -28,16 +33,56 @@ $( "body" ).keypress(function( event ) {
      event.preventDefault();
   }
 });
+function dziemba_levenshtein(a, b){
+	var tmp;
+	if (a.length === 0) { return b.length; }
+	if (b.length === 0) { return a.length; }
+	if (a.length > b.length) { tmp = a; a = b; b = tmp; }
 
+	var i, j, res, alen = a.length, blen = b.length, row = Array(alen);
+	for (i = 0; i <= alen; i++) { row[i] = i; }
 
-function levenshteinDistance (s, t) {
-    if (!s.length) return t.length;
-    if (!t.length) return s.length;
-
-    return Math.min(
-        levenshteinDistance(s.substr(1), t) + 1,
-        levenshteinDistance(t.substr(1), s) + 1,
-        levenshteinDistance(s.substr(1), t.substr(1)) + (s[0] !== t[0] ? 1 : 0)
-    ) + 1;
+	for (i = 1; i <= blen; i++) {
+		res = i;
+		for (j = 1; j <= alen; j++) {
+			tmp = row[j - 1];
+			row[j - 1] = res;
+			res = b[i - 1] === a[j - 1] ? tmp : Math.min(tmp + 1, Math.min(res + 1, row[j] + 1));
+		}
+	}
+	return res;
 }
 
+function levenshtein(a, b) {
+    if(a.length === 0) return b.length;
+    if(b.length === 0) return a.length;
+  
+    var matrix = [];
+  
+    // increment along the first column of each row
+    var i;
+    for(i = 0; i <= b.length; i++){
+      matrix[i] = [i];
+    }
+  
+    // increment each column in the first row
+    var j;
+    for(j = 0; j <= a.length; j++){
+      matrix[0][j] = j;
+    }
+  
+    // Fill in the rest of the matrix
+    for(i = 1; i <= b.length; i++){
+      for(j = 1; j <= a.length; j++){
+        if(b.charAt(i-1) == a.charAt(j-1)){
+          matrix[i][j] = matrix[i-1][j-1];
+        } else {
+          matrix[i][j] = Math.min(matrix[i-1][j-1] + 1, // substitution
+                                  Math.min(matrix[i][j-1] + 1, // insertion
+                                           matrix[i-1][j] + 1)); // deletion
+        }
+      }
+    }
+  
+    return matrix[b.length][a.length];
+  }
